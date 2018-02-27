b064b380888e842566cfa2800e86dad149332105
el <hylee101001@gmail.com>
2018-02-26 21:46:24 -0500

    dd

--- main.py
+++ main.py
@@ -7,35 +7,34 @@ import cv2
 from keras.callbacks import EarlyStopping
 import pickle
 
-def runTrainCNN():
-    fn = getModel(360, 640)
-    fn.load_weights('Weights/b3_evenlight.h5')
-    of, vel, pos, DCM, img1, img2 = getMergedData([0, 2, 6, 4])
-    earlystop = EarlyStopping(monitor='loss', min_delta=10**-5, patience=5, verbose=1, mode='auto')
+def runTrain():
+    of, vel, pos, DCM, img1, img2 = getMergedData([1])
+
+    # dummy = np.zeros_like(vel)
+    # vel = np.concatenate((dummy, vel), axis = 1)
+
+    m = getCNN(320, 1152)
+    m.load_weights('Weights/temp_sg_try3.h5')
+    earlystop = EarlyStopping(monitor='dense_4_loss', min_delta=10**-6, patience=5, verbose=1, mode='auto')
     callbacks_list = [earlystop]
-    history = fn.fit([img1, img2, DCM], vel, epochs=10, batch_size=32, verbose=1,  shuffle=True, callbacks=callbacks_list)
-    fn.save_weights('Weights/b3_evenlight.h5')
+    history = m.fit([img1, img2, DCM], [of, vel], epochs=20, batch_size=10, verbose=1,  shuffle=True, callbacks=callbacks_list)
+    m.save_weights('Weights/temp_sg_try3.h5')
     print 'done'
 
-
 def runTest():
-    m = getModel(360, 640)
-    m.load_weights('Weights/b3_evenlight.h5')
-    runTestSeq(m,6)
-    for seq in range(0,11):
-        runTestSeq(m,seq)
-
-
-def runTestSeq(m,seq):
-    of, vel, pos, DCM, img1, img2 = getMergedData([seq])
+    m = getCNN(320, 1152)
+    m.load_weights('Weights/temp_sg_try3.h5')
+    of, vel, pos, DCM, img1, img2 = getMergedData([1])
     pred_vel_list = []
+
     i = 0
     while i < img1.shape[0]:
         inputImg1 = img1[i:i+10,:,:,:]
         inputImg2 = img2[i:i+10,:,:,:]
         dcm = DCM[i:i+10,:]
         pred = m.predict([inputImg1, inputImg2, dcm])
-        pred_vel_list.append(pred)
+        pred_vel = pred[1]
+        pred_vel_list.append(pred_vel)
         i += 10
         if i%100 == 0:
             print i
@@ -46,38 +45,31 @@ def runTestSeq(m,seq):
     #pred_vel = pred_vel[:,3:]
     diff = vel-pred_vel
 
-    print pred_vel.shape
-    print np.mean(diff)
-
     pred_pos = pos2vel(pred_vel)
     rmse = np.sqrt((np.asarray((np.subtract(pred_vel, vel))) ** 2).mean())
     print 'rmse=%f' %(rmse)
     rmse = np.sqrt((np.asarray((np.subtract(pred_pos, pos))) ** 2).mean())
     print 'rmse=%f' %(rmse)
 
-
-    fig = plt.figure()
+    print pred_vel.shape
+    print np.mean(diff)
+    plt.figure()
     plt.plot(vel, 'r')
     plt.plot(pred_vel, 'b')
-    #plt.show()
-    fig.savefig('Results/seq' + str(seq) + '_vel.png')
+    plt.show()
 
 
     pred_pos = vel2pos(pred_vel)
-    fig = plt.figure()
+    plt.figure()
     plt.plot(pos[:,0], pos[:,2], 'ro')
     plt.plot(pred_pos[:,0], pred_pos[:,2], 'b.')
-    if seq == 4:
-        plt.xlim((-200, 200))
-    if seq == 6:
-        plt.xlim((-50, 50))
-    #plt.show()
-    fig.savefig('Results/seq' + str(seq) + '_pos.png')
+    plt.show()
+
 
 if __name__=='__main__':
     type = int(sys.argv[1])
     if type==0:
-        runTrainCNN()
+        runTrain()
     elif type==1:
         runTest()
 
